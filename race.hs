5bbb95c7287e32820170de9ac2a45db10a0afd31
cfreund <carlosfreund@googlemail.com>
2013-04-26 07:37:55 +0200

    validation works

--- race.hs
+++ race.hs
@@ -1,14 +1,18 @@
 -- Für den Prog-wettbewerb der FH.
 
+import Data.List (partition)
 
 data Direction = N | E | S | W | D
 
+type Pos = (Int,Int)
 
 type Gamefield = ([Checkpoint],[Booster])
-type Checkpoint = (Int,Int,Int,Int,Int) --x1,y1,x2,y2,points
+type Checkpoint = (Pos,Pos,Int) --x1,y1,x2,y2,points
 type Booster = Checkpoint -- They look the same actually.
 
-type Car = (Int,Int,Int,Int,Int) --xpos,ypox,xspeed,yspeed,energy
+type Car = (Pos,Pos,Int) --xpos,ypox,xspeed,yspeed,energy
+
+type Gamestate = (Car,Gamefield, Maybe Int) --Maybe Int -> If the last checkpoint has been reached.
 
 --A racer is a function able of creating Directions for a given gamefield.
 --Diffrent Racing-Strategies will be implemented that way.
@@ -17,36 +21,62 @@ type Racer = Gamefield -> [Direction]
 
 --Let the racer race in the gamefield. Return an int, if he makes it to the end.
 race :: Racer -> Gamefield -> Maybe Int
-race racer field = 
+race racer field = won
    where route = racer field
+         (car, field, won) = runRoute route field
 
 
-step :: (Car,GameField) -> Direction -> (Car, GameField)
-step (car, field) dir = (tankedcar, newfield)
+runRoute :: [Direction] -> Gamefield -> Gamestate
+runRoute route field = foldl step startstate route
+   where ((fstChk:rstChkPnts),bstrs) = field
+         startstate = case fstChk of --state is (Car,Gamefield) 
+            ((x1,y1),(x2,y2),en)
+                -> let xpos = (x1+x2) `div` 2
+                       ypos = (y1+y2) `div` 2
+                   in (((xpos,ypos),(0,0),en),(rstChkPnts,bstrs), Nothing)
+
+
+step :: Gamestate -> Direction -> Gamestate
+step (car, field, won) dir = (tankedcar, newfield, newwon)
    where movedcar = applyDirection car dir
          (tankedcar, newfield) = checkFields movedcar field
+         newwon = case won of 
+           Nothing -> case newfield of 
+             ([],_) -> case tankedcar of (_,_,en) -> Just en
+             x -> Nothing
+           x -> x
+            
 
 applyDirection :: Car -> Direction -> Car
-applyDirection (x,y,xs,ys,en) dir = move case dir of 
-     N -> (x,y-1,xs,ys,en)
-     E -> (x+1,y,xs,ys,en)
-     S -> (x,y+1,xs,ys,en)
-     W -> (x-1,y,xs,ys,en)
-     D -> (x,  y,xs,ys,en)
-
-move :: Car -> Car
-move (x,y,xs,ys,en) = (x+xs,y+ys,xs,ys,en-1)
-
-
-
+applyDirection (pos,speed,en) dir = (addPos pos newspeed, newspeed, en-1)
+   where newspeed = addPos speed (case dir of
+                      N -> ( 0,-1)
+                      E -> ( 1, 0)
+                      S -> ( 0,-1)
+                      W -> (-1, 0)
+                      D -> ( 0, 0))
+         
 checkFields :: Car -> Gamefield -> (Car,Gamefield)
-checkFields (x,y,xs,ys,en) (checkpoints, boosters) = 
-     ((x,y,xs,ys,newen) ,(unhitCp,unhitbst))
-   where (hitCp,unhitCp)   = partition (doesHit car) checkpoints
-         (hitBst,unhitBst) = partition (doesHit car) boosters
-         newen = foldr (\(_,_,_,_,e) o -> e+o) en (hitCp++hitBst)
+checkFields (p,s,en) (checkpoints, boosters) = 
+     ((p,s,newen) ,(unhitCp,unhitBst))
+   where (hitCp,unhitCp)   = partition (doesHit p) checkpoints
+         (hitBst,unhitBst) = partition (doesHit p) boosters
+         newen = foldr (\(_,_,e) a-> e+a) en (hitCp++hitBst)
          
 
-doesHit :: Car -> Checkpoint -> Bool
-doesHit (carx,cary,_,_,__) (cpx1,cpy1,cpx2,cpx2,_) = 
-   cpx1 <= carx && cary <= cpx2 && cpy1 <= cary && cary <= cpy2
+doesHit :: Pos -> Checkpoint -> Bool
+doesHit (x,y) ((cpx1,cpy1),(cpx2,cpy2),_) = 
+   cpx1 <= x && x <= cpx2 && cpy1 <= y && y <= cpy2
+
+
+strToRoute :: String -> [Direction]
+strToRoute = map (\c -> case c of 
+   'N' -> N 
+   'E' -> E
+   'S' -> S
+   'W' -> W
+   'D' -> D)
+
+
+addPos :: Pos -> Pos -> Pos
+addPos (x1,y1) (x2,y2) = (x1+x2,y1+y2)
