d3e9705e840db02698278a522db5035fd31bc7d0
cfreund <carlosfreund@googlemail.com>
2013-04-28 11:10:21 +0200

    added gitignore; works from whithin trackeditor

--- race/Run.hs
+++ race/Run.hs
@@ -8,23 +8,26 @@ racer = simple
 --race :: Racer -> Gamefield -> Gamestate
 
 inOut :: String -> String -> String
-inOut chks boosts = "de"
+inOut chks boosts = routeToStr route
    where gamefield = (parseCheckpoints chks, parseCheckpoints boosts)
+         route = racer gamefield
     
 
 main = do
          chks <- getLine
          boosts <-getLine
-         putStr (inOut chks boosts)
+         putStrLn (inOut chks boosts)
+         main
 
 
 parseCheckpoints :: String -> [Checkpoint]
 parseCheckpoints s = case result of 
       [(checkpoints,"")] -> checkpoints
       x -> error ("unexpected parse result: "++ show x)
-      
-   where result = norest (next3 (atom '[') (pKommaSep tupel) (atom ']')
+   where result = norest (next3 (atom '[') tupelOrNothing (atom ']')
                              (\   _            x                    _  -> x)) s
+         tupelOrNothing = alt (pKommaSep tupel) 
+                              (convert (const []) (maybeSome (atom ' ')))
          tupel = next3 (atom '(') (pKommaSep parseNumber) (atom ')') convertints
          convertints _ is _ = listToCheckpoint (map fromIntegral is)
          listToCheckpoint [x1,y1,x2,y2,en] = ((x1,y1),(x2,y2),en)
