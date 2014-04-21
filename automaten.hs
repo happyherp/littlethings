832468594873430a504e096bba7db8d20c2b2085
Carlos Freund <carlosfreund@googlemail.com>
2014-04-21 15:26:22 +0200

    umstellung auf sets

--- automaten.hs
+++ automaten.hs
@@ -1,5 +1,6 @@
 
- import Data.Set (Set)
+import Data.Set (Set, singleton, empty, union, unions, member, insert)
+import qualified Data.Set as Set
 
 
 data RegExp a = Terminal a 
@@ -8,14 +9,15 @@ data RegExp a = Terminal a
                | Repitition (RegExp a) 
    deriving Show
    
-matches :: Eq a => (RegExp a) -> [a] -> Bool
-matches r s =  elem [] (matchPart r s)
+matches :: Ord a => (RegExp a) -> [a] -> Bool
+matches r s =  member [] (matchPart r s)
 
-matchPart ::  Eq a => (RegExp a) -> [a] -> [[a]]
-matchPart (Terminal t) (x:xs) | t == x = [xs]
-matchPart (Terminal _) _ = []
-matchPart (Sequence r1 r2) xs = concatMap (matchPart r2) (matchPart r1 xs )
-matchPart (Alternative r1 r2) xs = matchPart r1 xs ++ matchPart r2 xs
-matchPart (Repitition r) xs = [xs] ++ matchPart (Sequence r (Repitition r)) xs
+matchPart ::  Ord a => (RegExp a) -> [a] -> Set [a]
+matchPart (Terminal t) (x:xs) | t == x = singleton xs
+matchPart (Terminal _) _ = empty
+matchPart (Sequence r1 r2) xs = unions $ map (matchPart r2) 
+                                             (Set.toList (matchPart r1 xs))
+matchPart (Alternative r1 r2) xs = union (matchPart r1 xs) (matchPart r2 xs)
+matchPart (Repitition r) xs = insert xs (matchPart (Sequence r (Repitition r)) xs)
 
-r1 = Sequence (Terminal "1") (Repitition (Terminal "0"))
+r1 = Sequence (Terminal '1') (Repitition (Terminal '0'))
