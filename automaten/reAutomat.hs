4c4f4dfb67a7cfe530ea778c5a8f43847f2c51e9
Carlos Freund <carlosfreund@googlemail.com>
2014-04-25 12:28:15 +0200

    reToEA

--- automaten/reAutomat.hs
+++ automaten/reAutomat.hs
@@ -8,9 +8,21 @@ toDEA (Sequence r1 r2) =
   let (q1, e1, s1, i1, f1) = (toDEA r1)
       (q2, e2, s2, i2, f2) = makeDisjunct (toDEA r1) (toDEA r2)
       sNew =((head f1, [], head i2) : union s1 s2)
-  in (union q1 q2, union e1 e2, sNew, i1, f2)
-    
-                              
+  in (union q1 q2, union e1 e2, sNew, i1, f2)  
+toDEA (Alternative r1 r2) =     
+  let (q1, e1, s1, i1, f1) = (toDEA r1)
+      (q2, e2, s2, i2, f2) = makeDisjunct (toDEA r1) (toDEA r2)
+      iNew = maximum (union q1 q2) + 1
+      fNew = iNew + 1
+      qNew = union q1 q2 ++ [iNew, fNew]
+      sNew = union s1 s2 ++ [(iNew, [], head i1), (iNew, [], head i2), 
+                             (head f1, [], fNew), (head f2, [], fNew)] 
+   in (qNew, union e1 e2, sNew, [iNew], [fNew])
+toDEA (Repetition r) = 
+  let (q, e, s, i, f) = toDEA r
+      fNew = maximum q + 1
+      sNew = s ++ [(head i, [], fNew), (head f, [], fNew), (fNew, [], head i)]
+  in (q ++ [fNew], e, sNew, i, [fNew])
 
 
 
