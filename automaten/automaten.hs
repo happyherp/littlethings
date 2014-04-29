5107484ea82cbca84083383d0351a33161ed38a7
Carlos Freund <carlosfreund@googlemail.com>
2014-04-29 06:13:18 +0200

    more things?

--- automaten/automaten.hs
+++ automaten/automaten.hs
@@ -1,6 +1,9 @@
 module Automaten where
 
+import Data.List
 import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+
 
 type Q  a = [a]  --Alle zustände
 type Z b = [b]  --Eingabealphabet
@@ -11,13 +14,50 @@ type Wort b = [b]
 
 type EA a b = (Q a,Z b, S a b, I a, F a) --Automat
 
+
 isValidAutomat :: Ord a => Ord b => EA a b -> Bool
 isValidAutomat (q,z,s,i,f) = 
   i `isSubsetOf` q && f `isSubsetOf` q 
   && all (\(p,a,r) -> p `elem` q && all (\e -> elem e z) a && r `elem` q) s
-                                 
-                                 
-                                 
+   
+isDEA :: Ord a => Ord b => EA a b -> Bool
+isDEA (q, e, s, i, f) = isValidAutomat (q, e, s, i, f) && length i == 1 
+                        && isSpelling (q, e, s, i, f)
+                        && not (hasMultipaths (q, e, s, i, f))
+                        
+isSpelling :: Ord a => Ord b => EA a b -> Bool
+isSpelling (q, e, s, i, f) =  all (\(_, a, _) -> length a == 1) s   
+
+hasMultipaths :: Ord a => Ord b => EA a b -> Bool
+hasMultipaths (q, e, s, i, f) =  any (\(p,a,q) -> any (\(p2,a2,q2)->(p,a) == (p2,a2) && q /= q2) s) s             
+
+--Transform a NEA to a DEA. Does not do remove transitions with words > 1 
+-- or multiple start states                        
+toDEA :: Enum a => Ord a => Ord b => EA a b -> EA a b
+toDEA ea | isDEA ea = ea
+toDEA ea | any (\(q,a,p)-> q == p && a == []) s = (q,e,sNew,i,f)
+  where (q,e,s,i,f) = ea 
+        sNew = filter (\(q,a,p)-> q /= p && a /= []) s
+toDEA ea | not (isSpelling ea) = 
+  let (q,e,s,i,f) = ea
+      (p,_,r) = fromJust $ find (\(_,a,_) -> a == []) s
+      newTrans = map (\(_,a,t)->(p,a,t)) $ filter (\(t,_,u) -> t == r ) s
+      sNew = nub $ newTrans ++ (delete (p, [], r) s)
+      fNew = if r `elem` f then (p:f) else f
+  in toDEA (q,e,sNew,i,fNew)
+toDEA ea | hasMultipaths ea = 
+  let (q,e,s,i,f) = ea
+      trans = head $ filter (\xs -> length xs > 1) (groupBy (\(q,a,_) (r,b,_) -> (q,a) == (r,b)) s)
+      transStates = map (\(_,_,q)-> q) trans
+      newstate = succ $ maximum q
+      (p, a, _) = head trans
+      newtrans = map (\(_,a,r) -> (newstate, a, r)) $filter (\(p,_,_) -> elem p transStates) s
+      sNew = (s \\ trans) ++ [(p,a,newstate)] ++ newtrans
+      fNew = if any (\p -> elem p f) transStates then (newstate:f) else f
+  in toDEA ((newstate:q), e, sNew, i, fNew)
+        
+
+                                                        
 
 a1 :: EA Char Int
 a1 = ("abcde", [1..3], [('a', [1], 'b'),
@@ -34,8 +74,21 @@ a1 = ("abcde", [1..3], [('a', [1], 'b'),
 
 a2 :: EA Char Int
 a2 = ( "a", [1],[('a', [1], 'a')], "a", "a")   
+a3 :: EA Char Int
+a3 = ( "a", [],[('a', [], 'a')], "a", "a")   
 
-
+a4 :: EA Char Int
+a4 = ("abcde", [1..3], [('a', [1], 'b'),
+                        ('a', [2], 'c'),
+                        ('a', [], 'e'),
+                        ('b', [1], 'b'),
+                        ('b', [3], 'e'),
+                        ('e', [1], 'a'),
+                        ('e', [1], 'b'),
+                        ('e', [2], 'c'),
+                        ('a', [2], 'c'),
+                        ('b', [2], 'c')], "a", "be")     
+                        
 --helpers
 isSubsetOf a b = (Set.fromList a) `Set.isSubsetOf` (Set.fromList b)
      
--- automaten/reAutomat.hs
+++ automaten/reAutomat.hs
@@ -1,25 +1,26 @@
 import Re
 import Automaten
 import Data.List
+import Data.Maybe (fromJust)
 
-toDEA :: Eq a => RegExp a -> EA Integer a
-toDEA (Terminal x) = ([1,2], [x], [(1,[x],2)], [1], [2]) 
-toDEA (Sequence r1 r2) = 
-  let (q1, e1, s1, i1, f1) = (toDEA r1)
-      (q2, e2, s2, i2, f2) = makeDisjunct (toDEA r1) (toDEA r2)
+toNEA :: Eq a => RegExp a -> EA Integer a
+toNEA (Terminal x) = ([1,2], [x], [(1,[x],2)], [1], [2]) 
+toNEA (Sequence r1 r2) = 
+  let (q1, e1, s1, i1, f1) = (toNEA r1)
+      (q2, e2, s2, i2, f2) = makeDisjunct (toNEA r1) (toNEA r2)
       sNew =((head f1, [], head i2) : union s1 s2)
   in (union q1 q2, union e1 e2, sNew, i1, f2)  
-toDEA (Alternative r1 r2) =     
-  let (q1, e1, s1, i1, f1) = (toDEA r1)
-      (q2, e2, s2, i2, f2) = makeDisjunct (toDEA r1) (toDEA r2)
+toNEA (Alternative r1 r2) =     
+  let (q1, e1, s1, i1, f1) = (toNEA r1)
+      (q2, e2, s2, i2, f2) = makeDisjunct (toNEA r1) (toNEA r2)
       iNew = maximum (union q1 q2) + 1
       fNew = iNew + 1
       qNew = union q1 q2 ++ [iNew, fNew]
       sNew = union s1 s2 ++ [(iNew, [], head i1), (iNew, [], head i2), 
                              (head f1, [], fNew), (head f2, [], fNew)] 
    in (qNew, union e1 e2, sNew, [iNew], [fNew])
-toDEA (Repetition r) = 
-  let (q, e, s, i, f) = toDEA r
+toNEA (Repetition r) = 
+  let (q, e, s, i, f) = toNEA r
       fNew = maximum q + 1
       sNew = s ++ [(head i, [], fNew), (head f, [], fNew), (fNew, [], head i)]
   in (q ++ [fNew], e, sNew, i, [fNew])
@@ -31,9 +32,7 @@ toDEA (Repetition r) =
 makeDisjunct :: EA Integer a -> EA Integer a -> EA Integer a
 makeDisjunct (q1,_,_,_,_) (q2, e2, s2, i2, f2) = 
    let mapping = zip q2 (filter (\i -> notElem i q1) [1..])
-       newState q = case lookup q mapping of 
-                     Nothing -> error "unknown state"
-                     Just qn -> qn
+       newState q = fromJust $ lookup q mapping
    in (map newState q2, e2, 
        map (\(p,a,q) -> (newState p, a, newState q)) s2,
        map newState i2, map newState f2)
