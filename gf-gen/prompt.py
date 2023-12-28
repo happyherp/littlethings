6c391450c246cae41785c0ccba5fca867c7fc280
Carlos Freund <carlosfreund@googlemail.com>
2023-12-28 15:29:13 +0100

    prompt improvements

--- gf-gen/prompt.py
+++ gf-gen/prompt.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+NEUTRAL_POSE = "https://cdn.discordapp.com/attachments/1189435958400131072/1189921466989432903/carlosamigo__a_mannequinn_standing_straight_arms_folded_all_whi_35a7af27-92a0-4e1e-8ded-499e1970f841.png?ex=659febb9&is=658d76b9&hm=fe18bcbaada51cbf26f0f9e541d72885519a57124a9801330e341958d6fe986e&"
+
 @dataclass
 class Person:
     age: int = 30
@@ -10,12 +12,35 @@ class Person:
     attractiveness: float = 1.0  # a float representing attractiveness
     
     def createPrompt(self) -> str:
-        prompt = f"/imagine a {self.age} year old {self.gender} with {self.hair} hair, "
-        prompt += f"of {self.ethnicity} descent ::1 "
+        nos = ["airbrush", "filter", "make up"]
+        
+        #basic
+        prompt = f"/imagine \n{NEUTRAL_POSE} a {self.age} year old {self.gender} with {self.hair} hair, "
+        prompt += f"of {self.ethnicity} descent, "
+        prompt += "wearing jeans and a white T-Shirt and sneakers, "
+        prompt += "full body, making eye contact, hands, "
+        prompt += "photoshoot, Studio Lighting, "
+        prompt += " ::1 "
+        #prompt += "shoes :: 2 face :: 2 " #force full body
+        
+        #weighted
         if (self.fatness > 1.0):
             prompt += f"overweight ::{self.fatness-1.0} "
         elif (self.fatness < 1.0): 
             prompt += f"underweight ::{1.0-self.fatness} "
+        if (self.attractiveness > 1.0):
+            prompt += f"attractive ::{self.attractiveness-1.0} "
+        elif (self.attractiveness < 1.0): 
+            prompt += f"ugly ::{1.0-self.attractiveness} "  
+            nos.append("beautiful")
+
+        #parameters
+        prompt += "--style raw "
+        prompt += "--ar 1:2 "
+        if (len(nos) > 0): 
+            prompt += "--no " + ", ".join(nos)
+        prompt += "--seed 654654 "
+        prompt += "--stylize 0 "
         return prompt
 
 
@@ -30,3 +55,5 @@ print("fatKid", fatKid.createPrompt());
 skinnyGrandma = Person(age=80, fatness=0.5)
 print("skinnyGrandma:", skinnyGrandma.createPrompt())
 
+ugly18 = Person(age=18, attractiveness=0.0)
+print("ugly18:", ugly18.createPrompt())
